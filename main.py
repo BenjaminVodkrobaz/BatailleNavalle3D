04a11da1d435666692c6ea224ac9e69b2f93599f
BenjaminVodkrobaz <b.nicolas560@laposte.net>
2021-12-02 10:04:55 +0100

    premiers fichiers

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import plateau
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Game:
+    def __init__(self):
+        self.gameInit()
+        self.gameLoop()
 
+    def gameInit(self):
+        print("Entrez le nom du joueur A")
+        joueurA = input()
+        print("Entrez le nom du joueur B")
+        joueurB = input()
+    def gameLoop(self):
+        pass
+    def gameEnd(self):
+        pass #TODO définir comment réinitialiser la game
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__=="__main__":
+    game = Game()
---
+++ plateau.py
@@ -0,0 +1,8 @@
+
+
+class Plateau:
+    def __init__(self):
+        self.initGrille()
+
+    def initGrille(self):
+        pass
