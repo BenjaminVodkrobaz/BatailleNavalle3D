cde4ef6bfa85e7be74a08b6fcd76d283dfbcee8d
BenjaminVodkrobaz <b.nicolas560@laposte.net>
2022-02-04 16:28:52 +0100

    stash des changes

--- sens.py
+++ sens.py
@@ -7,3 +7,14 @@ class Sens(Enum):
     BAS = 1 
     GAUCHE = 2 
     DROITE = 3
+
+    @staticmethod
+    def from_str(label):
+        if label.lower() in ('sens.bas', 'bas'):
+            return Sens.BAS
+        if label.lower() in ('sens.droite', 'droite'):
+            return Sens.DROITE
+        if label.lower() in ('sens.haut', 'haut'):
+            return Sens.HAUT
+        if label.lower() in ('sens.gauche', 'gauche'):
+            return Sens.GAUCHE
